bb59b9b9e655380544f2680ba2725a9b981881a2
Taeho <rjsdnxogh55@gmail.com>
2023-08-02 01:05:45 +0900

    feat: Modify ExceptionHandler & ValidList
    - json으로 파싱이 불가능한 경우의 예외처리를 명시했습니다.
    - 입력에서 빈 배열이 들어올 경우의 예외처리를 추가했습니다.

--- src/main/java/com/example/kakao/_core/errors/GlobalExceptionHandler.java
+++ src/main/java/com/example/kakao/_core/errors/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.kakao._core.errors.exception.*;
 import com.example.kakao._core.utils.ApiUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -35,8 +36,15 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(e.body(), e.status());
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> inValidInput(Exception e) {
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.error("정상적인 입력이 아닙니다.", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(apiResult, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> unknownServerError(Exception e){
+        e.printStackTrace();
         ApiUtils.ApiResult<?> apiResult = ApiUtils.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(apiResult, HttpStatus.INTERNAL_SERVER_ERROR);
     }
--- src/main/java/com/example/kakao/_core/utils/ValidList.java
+++ src/main/java/com/example/kakao/_core/utils/ValidList.java
@@ -4,13 +4,16 @@ import lombok.Data;
 import lombok.experimental.Delegate;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class ValidList<E> implements List<E> {
 
-    @Valid
     @Delegate
+    @NotEmpty(message = "데이터가 비어있습니다.")
+    @Valid
     private List<E> validList = new ArrayList<>();
 }
